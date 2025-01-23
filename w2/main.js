305bfa00df93ab4f199d9b6278e9cade202d48b9
potasium2 <noahsaber64@aol.com>
2025-01-23 15:37:15 -0500

    coding is done

--- w2/main.js
+++ w2/main.js
@@ -1,1 +1,17 @@
 console.log("JS Script Loaded");
+
+const houseMembersImpact = 8;
+const houseSizeImpact = 7;
+const foodChoiceImpact = 14;
+const waterConsumptionImpact = 3;
+const householdPurchaseImpact = 2;
+const wasteProduceImpact = 20;
+const recyclingWastImpact = 4;
+const transportationImpact = 16;
+
+const totalCarbonImpact = houseMembersImpact + houseSizeImpact + 
+foodChoiceImpact + waterConsumptionImpact + householdPurchaseImpact +
+wasteProduceImpact + recyclingWastImpact + transportationImpact;
+
+const myHeading = document.querySelector("span");
+myHeading.textContent = totalCarbonImpact;
