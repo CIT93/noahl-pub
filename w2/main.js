aed0e8a343e2df7cfdbce9398dab35dd9762b046
potasium2 <noahsaber64@aol.com>
2025-01-23 16:40:27 -0500

    coding is done v2
    Added comments cause I FORGOT 🇩🇪

--- w2/main.js
+++ w2/main.js
@@ -1,17 +1,19 @@
 console.log("JS Script Loaded");
 
-const houseMembersImpact = 8;
-const houseSizeImpact = 7;
-const foodChoiceImpact = 14;
-const waterConsumptionImpact = 3;
-const householdPurchaseImpact = 2;
-const wasteProduceImpact = 20;
-const recyclingWastImpact = 4;
-const transportationImpact = 16;
+const houseMembersImpact = 8; // Impact from number of people I live with
+const houseSizeImpact = 7; // Impact from the size of my house
+const foodChoiceImpact = 14; // Impact from the food I eat
+const waterConsumptionImpact = 3; // Impact from dishwasher/washing machine
+const householdPurchaseImpact = 2; // Impact from furnature purchases
+const wasteProduceImpact = 20; // Impact from trash thrown away
+const recyclingWastImpact = 4; // Impact from recycling
+const transportationImpact = 16; // Impact from driving/flying
 
+// Total Carbon Footprint
 const totalCarbonImpact = houseMembersImpact + houseSizeImpact + 
 foodChoiceImpact + waterConsumptionImpact + householdPurchaseImpact +
 wasteProduceImpact + recyclingWastImpact + transportationImpact;
 
+// Run accross span so it keeps h1 size without directly affecting the rest of the h1 tag
 const myHeading = document.querySelector("span");
 myHeading.textContent = totalCarbonImpact;
