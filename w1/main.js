f56c8fb2bbc5360c9fdf6c99d0c01d37ebabc75c
potasium2 <noahsaber64@aol.com>
2025-01-27 18:59:27 -0500

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,20 @@
-console.log("Week 1 - Hello World");
+console.log("hello from inside main.js");
+
+//let myVar;
+
+const myVar = "Noah";
+const myVarType = typeof(myVar);
+
+console.log("myVarType " + myVarType);
+console.log(`myVarType ${myVarType}`);
+
+function runNow () {
+    if (myVarType === "number") {
+        console.log(`will this line (12) run`);
+    } else {
+        console.log(`will this line (14) run`);
+    }
+}
+
+runNow();
+runNow();
