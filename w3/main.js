a9ba083fd386c1e8951a151fee92fa4e828d527f
potasium2 <noahsaber64@aol.com>
2025-01-30 20:31:56 -0500

    working code
    variable == value ? do thing : do other thing

--- w3/main.js
+++ w3/main.js
@@ -1,32 +1,59 @@
-function carbonFootprintCalculator(numberInHousehold) {
+let houseHoldCarbonImpact = 0;
+
+function houseHoldCarbonCalculator(numberInHousehold) {
+    // Set carbon impact so it doesn't infinitely add
     if (numberInHousehold === 1) {
-        carbonFootprint += 14;
+        houseHoldCarbonImpact = 14;
     }
     else if (numberInHousehold === 2) {
-        carbonFootprint += 12;
+        houseHoldCarbonImpact = 12;
     }
     else if (numberInHousehold === 3) {
-        carbonFootprint += 10;
+        houseHoldCarbonImpact = 10;
     }
     else if (numberInHousehold === 4) {
-        carbonFootprint += 8;
+        houseHoldCarbonImpact = 8;
     }
     else if (numberInHousehold === 5) {
-        carbonFootprint += 6;
+        houseHoldCarbonImpact = 6;
     }
     else if (numberInHousehold === 6) {
-        carbonFootprint += 4;
+        houseHoldCarbonImpact = 4;
     }
     else if (numberInHousehold > 6) {
-        carbonFootprint += 2;
+        houseHoldCarbonImpact = 2;
     }
     else {
         console.log("Points did not update"); // Edgecase
     }
 
-    console.log(`With a household of ${numberInHousehold} members, the total carbon footprint would equate to roughly ${carbonFootprint} points`);
+    console.log(`With a household of ${numberInHousehold} members, the estimated carbon points score would equate to ${houseHoldCarbonImpact} points`);
 }
 
-let carbonFootprint = 0;
-carbonFootprintCalculator(3);
-carbonFootprintCalculator(4);
+houseHoldCarbonCalculator(3);
+houseHoldCarbonCalculator(4);
+
+let houseSizeCarbonImpact = 0;
+function houseSizeCarbonCalculator(houseSize) {
+    if (houseSize === "Large") {
+        houseSizeCarbonImpact = 10;
+    }
+    else if (houseSize === "Medium") {
+        houseSizeCarbonImpact = 7;
+    }
+    else if (houseSize === "Small") {
+        houseSizeCarbonImpact = 4;
+    }
+    else if (houseSize === "Apartment") {
+        houseSizeCarbonImpact = 2;
+    }
+    else {
+        console.log("Points did not update"); // Edgecase
+    }
+
+    // Comparison so grammer is correct :)
+    console.log(`With ${houseSize === "Apartment" ? "an Apartment" : `a ${houseSize} house`}, the estimated carbon points score would equate to ${houseSizeCarbonImpact} points`);
+}
+
+houseSizeCarbonCalculator("Large");
+houseSizeCarbonCalculator("Apartment");
