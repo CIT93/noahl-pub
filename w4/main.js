03cd2463de82da10c932d47eb0684885d2ff0bf8
potasium2 <noahsaber64@aol.com>
2025-02-03 20:29:31 -0500

    functions with return values

--- w4/main.js
+++ w4/main.js
@@ -1,4 +1,5 @@
 function houseSizeImpact(size) {
+    console.log("Inside Block");
     if (size === "large") {
         carbonFootprintPoints += 10;
     } else if (size === "medium") {
@@ -16,36 +17,42 @@ function houseSizeImpact(size) {
 }
 
 function houseHoldImpact(numberInHousehold) {
+    console.log("Inside Block");
+    let houseHoldPoints = 0;
     if (numberInHousehold === 1) {
-        carbonFootprintPoints += 14;
+        houseHoldPoints = 14;
     }
     else if (numberInHousehold === 2) {
-        carbonFootprintPoints += 12;
+        houseHoldPoints = 12;
     }
     else if (numberInHousehold === 3) {
-        carbonFootprintPoints += 10;
+        houseHoldPoints = 10;
     }
     else if (numberInHousehold === 4) {
-        carbonFootprintPoints += 8;
+        houseHoldPoints = 8;
     }
     else if (numberInHousehold === 5) {
-        carbonFootprintPoints += 6;
+        houseHoldPoints = 6;
     }
     else if (numberInHousehold === 6) {
-        carbonFootprintPoints += 4;
+        houseHoldPoints = 4;
     }
     else if (numberInHousehold > 6) {
-        carbonFootprintPoints += 2;
+        houseHoldPoints = 2;
     }
     else {
-        console.log("Points did not update"); // Edgecase
-        return;
+        return houseHoldPoints; // In the case of Edgecase
     }
-    console.log(`The carbon footprint has updated to ${carbonFootprintPoints}`)
+    console.log(`With a household of ${numberInHousehold} members, the carbon footprint would be equal to ${houseHoldPoints} points`)
+    return houseHoldPoints;
 }
 
-let carbonFootprintPoints = 0;
-// The console will not log anything
+console.log("Global Scope");
+function start() {
+    const houseHoldPts = houseHoldImpact(5);
+    //houseSizeImpact("large");
+}
 
-houseHoldImpact(5);
-houseSizeImpact("large");
+start();
+
+// Refactor the codebase so it's using less variables to make functions more easily repeatable
