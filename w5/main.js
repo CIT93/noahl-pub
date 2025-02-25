889b4f29e67589bb41181eb3da0aaef298bbe87f
potasium2 <noahsaber64@aol.com>
2025-02-25 02:31:36 -0500

    my code
    yeah

--- w5/main.js
+++ w5/main.js
@@ -1,28 +1,37 @@
-const movieArray = [];
+const moviesArray = []; // Keeping the array and function for readability and usability
 
-function addMovie(movieTitle, movieReleaseYear, movieRating) {
+function addMovie(movieTitle, movieReleaseYear, movieRating, timesWatched) {
     const movieObj = {
-        movieTitle: movieTitle,
-        movieReleaseYear: movieReleaseYear,
-        movieRating: movieRating,
+        title: movieTitle,
+        releaseYear: movieReleaseYear,
+        rating: movieRating,
+        watched: timesWatched
     };
 
-    movieArray.push(movieObj);
+    moviesArray.push(movieObj);
 }
 
-function displayMovies() {
+function displayMovies(movies) {
     const output = document.getElementById("output");
 
-    for(movieObj of movieArray) {
+    // They made foreach a lambda???
+    movies.forEach(movie => {
         const newH2 = document.createElement("h2");
-        newH2.textContent = `${movieObj.movieTitle} (${movieObj.movieReleaseYear}) has a rating of ${movieObj.movieRating}/10`;
-
+        if (movie.rating >= 7 && movie.watched >= 3) {
+            newH2.textContent = `I have watched ${movie.title} (${movie.releaseYear}) ${movie.watched} times and give it a rating of ${movie.rating}/10`;
+        } else {
+            newH2.textContent = `N/A` // idk
+        }
         output.appendChild(newH2);
-    }
+    });
 }
 
-addMovie("Interstellar", 2014, 9);
-addMovie("The Martian", 2015, 9);
-addMovie("El Camino", 2019, 8);
-addMovie("Moonfall", 2021, 4);
-displayMovies();
+// I rarely watch movies idk what to put
+addMovie("Interstellar", 2014, 10, 4);
+addMovie("The Martian", 2015, 9, 8);
+addMovie("Doctor Strange", 2016, 8, 6);
+addMovie("El Camino", 2019, 7, 2);
+addMovie("The Time Machine", 1960, 7, 11);
+addMovie("Black Widow", 2021, 4, 1);
+
+displayMovies(moviesArray);
