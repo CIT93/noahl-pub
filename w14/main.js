6f050eb4d6a0012d86d07f951be864e8ec55c921
potasium2 <noahsaber64@aol.com>
2025-04-24 08:28:18 -0400

    w14 my attempt at the code, before I review other students or solution code

---
+++ w14/main.js
@@ -0,0 +1,23 @@
+function displayImg(photos) {
+    photos.forEach(photo => {
+        const img = document.createElement("img");
+        img.setAttribute("src", photo.thumbnailUrl);
+        document.getElementById("thumbnail-output").appendChild(img);
+    });
+}
+
+async function start() {
+    try {
+        const data = await fetch("https://jsonplaceholder.typicode.com/photos");
+        const photoArray = await data.json();
+        displayImg(photoArray);
+    } catch(e) {
+        const paragraph = document.createElement("p");
+        paragraph.innerHTML = "uh oh couldnt fetch server !!!";
+        document.getElementById("thumbnail-output").appendChild(paragraph);
+
+        console.log(`Error: ${e}`);
+    }
+}
+
+start();
